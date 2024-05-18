9f6917250756778afceaa01bbd8e587268ee4db3
Mert Öztürk <mert32139@gmail.com>
2024-05-18 21:23:57 +0300

    update to student table

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,44 +1,44 @@
-import { useState} from "react"
+import React, { useState } from 'react';
+import { supabase } from '../supaClient';
 
-export default function Register(){
+export default function Register() {
+  const [count, setCount] = useState({});
+  console.log({ supabase });
 
-    const [count, setCount] = useState([])
-    const SomeComponent = () => {
-        const supabase = useSupabase();
-        
-        console.log(supabase);
-    }
-    SomeComponent();      
-    const RegisterStudent = async(e) => {
-        e.preventDefault();
-        const datas = new FormData(e.target)
-        const dataEntries = Object.fromEntries(datas)
+  const RegisterStudent = async (e) => {
+    e.preventDefault();
+    const datas = new FormData(e.target);
+    const dataEntries = Object.fromEntries(datas);
+    console.log(dataEntries);
+    const { data, error } = await supabase
+      .from('students')
+      .insert([
+        {
+          student_name: dataEntries.student_name,
+          student_surname: dataEntries.student_surname,
+          student_number: dataEntries.student_number,
+          student_lesson: dataEntries.student_lesson, 
+        },
+      ]);
+    
+      setCount(data);
+  };
+  
 
-        const { data, error } = await _supabase
-        .from('students')
-        .insert([
-            {
-                student_name: dataEntries.student_name,
-                student_surname: dataEntries.student_surname,
-                student_number: dataEntries.stemail_address,
-                student_lesson: dataEntries.student_id,
-            }
-        ]);
-        setCount(data)
-    }
-    console.log(count);
-    return(
-       <div className="regformsCont">
-            <form className="register" onSubmit={RegisterStudent}>
-                    <input type="text" name="student_name" placeholder="Öğrenci Adı"/>
-                    <input type="text" name="student_surname" placeholder="Öğrenci Soyadı " />
-                    <input type="text" name="student_number" placeholder="Öğrenci Numarası"/>
-                    <select id="classLesson">Dersi:
-                        <option value="FrontEnd">FrontEnd</option>
-                        <option value="Backend">Backend</option>
-                    </select>
-                    <input type="submit" placeholder="Gönder"/>
-            </form>
-       </div>
-    )
+  console.log(count);
+
+  return (
+    <div className="regformsCont">
+      <form className="register" onSubmit={RegisterStudent}>
+        <input type="text" name="student_name" placeholder="Öğrenci Adı" />
+        <input type="text" name="student_surname" placeholder="Öğrenci Soyadı " />
+        <input type="text" name="student_number" placeholder="Öğrenci Numarası" />
+        <select name="student_lesson">
+          <option value="FrontEnd">FrontEnd</option>
+          <option value="Backend">Backend</option>
+        </select>
+        <input type="submit" placeholder="Gönder" />
+      </form>
+    </div>
+  );
 }
--- src/supaClient.jsx
+++ src/supaClient.jsx
@@ -2,9 +2,9 @@ import React, { createContext, useContext } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://tdnqsgnohrskrghypooc.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInRkbnFzZ25vaHJza3JnaHlwb29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MTM5MTcsImV4cCI6MjAzMTA4OTkxN30.VSc3RwzPlo2_Myk5-h2q-NkU1jz5Weeo9HmPaHMhh-g';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRkbnFzZ25vaHJza3JnaHlwb29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU1MTM5MTcsImV4cCI6MjAzMTA4OTkxN30.VSc3RwzPlo2_Myk5-h2q-NkU1jz5Weeo9HmPaHMhh-g';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const SupabaseContext = createContext();
 
@@ -13,5 +13,3 @@ export const SupabaseProvider = ({ children }) => (
     {children}
   </SupabaseContext.Provider>
 );
-
-export const useSupabase = () => useContext(SupabaseContext);
